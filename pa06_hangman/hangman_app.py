5982aa8c4d48fdf979fd53da75d00b63c565f46f
ztbartolome <ztbartolome@gmail.com>
2020-04-08 09:16:46 -0400

    Update hangman_app.py

--- pa06_hangman/hangman_app.py
+++ pa06_hangman/hangman_app.py
@@ -1,11 +1,15 @@
 """
+    Authors: Timothy Hickey, Zeline Tricia Bartolome
+
    hangman_app.py is an app for playing hangman in the terminal
    it is also used as a module in the hangman_webapp flask app
 """
+import random
+words = "this is a string of words".split()
 
 def generate_random_word():
    """ read a list of words from a file and pick a random one to return """
-   return "unimplemented"
+   return random.choice(words)
 
 def play_hangman():
    """ this is the python script version of the game """

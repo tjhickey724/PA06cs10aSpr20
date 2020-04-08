66f59d06be9ae7907c9a1da06fe04238d244c5d3
Timothy J Hickey <tjhickey@brandeis.edu>
2020-04-08 09:26:23 -0400

    create framework for game
    We cut/pasted in the code from the jupyter notebook and made it runnable, but there is much more to do...

--- pa06_hangman/hangman_app.py
+++ pa06_hangman/hangman_app.py
@@ -11,9 +11,38 @@ def generate_random_word():
    """ read a list of words from a file and pick a random one to return """
    return random.choice(words)
 
+    
 def play_hangman():
-   """ this is the python script version of the game """
-   print("The hangman app is under construction. Try again later!")
+    want_to_play = True
+
+
+    while (want_to_play):
+        guessed_letters = []
+        guesses_left = 6
+        word = "cat" #generate_random_word()
+        letter = 'a' #"ask the user for a letter"
+        done = False
+        while not done:
+            if letter in guessed_letters:
+                print("subtract one from guesses_left")
+                print("and tell them they already guessed that letter")
+
+            elif letter not in word:
+                print("""add letter to guessed letters"
+                "tell user the letter is not in the word"
+                "subtract one from the guesses_left" """)
+            else:
+                print(""" add letter to guessed letters"
+                "tell user the letter is in the word" """)
+            if  False: #"all the letters in the word have been guessed":
+                "set done to be true and tell the user they won!"
+            elif  False: #"the number of guesses left is zero":
+                "set done to be true and tell the user they lost!"
+            else:
+                print("print the word with a dash for each letter not in guessed_letters")
+                done = True
+                letter = 'e' # "ask the user for another letter"
+        want_to_play = False #"ask the user if they want to play another game..."
 
 if __name__ == '__main__':
     play_hangman()
